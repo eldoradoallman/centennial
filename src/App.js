a47299e4163bd6067b08c4feb45ae87b7818a8e0
Steffen Laurens <steffenlaurens@gmail.com>
2018-08-17 22:43:06 +0700

    add axios, redux-thunk, redux-logger & redux-promise-middleware

--- src/App.js
+++ src/App.js
@@ -1,31 +1,61 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import axios from 'axios';
 import logo from './logo.svg';
 
-const App = ({ store }) => (
-  <Provider store={store}>
+const FETCH_USERS = 'FETCH_USERS';
+
+const fetchUsersActionCreator = (actionType, apiUrl) => ({
+  type: actionType,
+  payload: axios.get(apiUrl)
+});
+
+const ConnectedApp = (props) => {
+  const { state, fetchUsers } = props;
+
+  return (
     <Router>
       <div className="App">
+        <img src={logo} width="150" style={{display:'block',margin:'auto'}} alt="React Logo" />
+        {state.users.users[0] ? <p>{state.users.users[0].name}</p> : ''}
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/topics">Topics</Link></li>
         </ul>
+        <button onClick={() => fetchUsers(state.users.users)}>CLICK ME</button>
 
         <hr />
 
-        <Route exact path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route path="/topics" component={Topics} />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+          <Route component={Home} />
+        </Switch>
       </div>
-    </Router>
-  </Provider>
-);
+    </Router>);
+};
 
-App.propTypes = {
-  store: PropTypes.object.isRequired
+ConnectedApp.propTypes = {
+  state: PropTypes.object.isRequired,
+  fetchUsers: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => {
+  return { state };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchUsers: (users) => {
+      if (users.length === 0) {
+        dispatch(fetchUsersActionCreator(FETCH_USERS, 'https://jsonplaceholder.typicode.com/users'));
+      }
+    }
+  };
 };
 
 const Home = () => (
@@ -63,5 +93,7 @@ const Topic = ({ match }) => (
     <h3>{match.params.topicId}</h3>
   </div>
 );
+
+const App = connect(mapStateToProps, mapDispatchToProps)(ConnectedApp);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,89 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+/* import axios from 'axios'; */
+import promise from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-const store = createStore(() => console.log('store created'));
+const CHANGE_NAME = 'CHANGE_NAME';
+const CHANGE_AGE = 'CHANGE_AGE';
+const FETCH_USERS_PENDING = 'FETCH_USERS_PENDING';
+const FETCH_USERS_REJECTED = 'FETCH_USERS_REJECTED';
+const FETCH_USERS_FULFILLED = 'FETCH_USERS_FULFILLED';
 
-ReactDOM.render(<App store={store} />, document.getElementById('root'));
+const userReducer = (state = {}, action) => {
+    switch (action.type) {
+        case CHANGE_NAME:
+            return { ...state, name: action.payload };
+        case CHANGE_AGE:
+            return { ...state, age: action.payload };
+        default:
+            return state;
+    }
+};
+
+const tweetsReducer = (state = [], action) => {
+    switch (action.type) {
+        case CHANGE_AGE:
+            return [ ...state, action.payload ];
+        default:
+            return state;
+    }
+};
+
+const initialState = {
+    fetching: false,
+    fetched: false,
+    users: [],
+    error: null
+};
+
+const usersReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case FETCH_USERS_PENDING:
+            return { ...state, fetching: true };
+        case FETCH_USERS_REJECTED:
+            return { ...state, fetching: false, error: action.payload };
+        case FETCH_USERS_FULFILLED:
+            return { ...state, fetching: false, fetched: true, users: action.payload.data };
+        default:
+            return state;
+    }
+};
+
+const appReducer = combineReducers({
+    user: userReducer,
+    tweets: tweetsReducer,
+    users: usersReducer
+});
+
+const middleware = applyMiddleware(promise(), thunk, logger);
+
+const store = createStore(appReducer, middleware);
+
+store.subscribe(() => store.getState());
+
+store.dispatch({
+    type: CHANGE_NAME,
+    payload: 'Steffen'
+});
+store.dispatch({
+    type: CHANGE_AGE,
+    payload: 32
+});
+store.dispatch({
+    type: CHANGE_AGE,
+    payload: 45
+});
+/* store.dispatch({
+    type: 'FETCH_USERS',
+    payload: axios.get("https://jsonplaceholder.typicode.com/users")
+}); */
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
