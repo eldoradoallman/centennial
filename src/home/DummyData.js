eb7e5d4a06771517bcacedef4dc8599459f96bff
Steffen Laurens <steffenlaurens@gmail.com>
2018-08-26 15:48:25 +0700

    complete featured news at home page

--- src/home/DummyData.js
+++ src/home/DummyData.js
@@ -52,6 +52,78 @@ const jsonData = [
     date: '5 Agustus 2018',
     topics: ['Entertainment'],
     url: '/entertainment/setelah-bumblebee-paramount-pictures-berencana-garap-film-optimus-prime'
+  },
+  {
+    id: '4',
+    title: 'KEREN! Game PUBG Tembus 400 Juta Pemain Guys. Kamu Main Juga Gak?',
+    image: {
+      size: {
+        small: 'https://steffen-laurens.com/centennial/img/img-highlight-news-4.jpg',
+        medium: 'https://steffen-laurens.com/centennial/img/img-highlight-news-4.jpg'
+      },
+      caption: 'Game PUBG"'
+    },
+    writer: {
+      id: 'w4',
+      name: 'Alex Sasongko'
+    },
+    date: '4 Agustus 2018',
+    topics: ['Games'],
+    url: '/entertainment/games/keren-game-pubg-tembus-400-juta-pemain-guys-kamu-main-juga-gak'
+  },
+  {
+    id: '5',
+    title: 'Penasaran Oppo Find X? Tunggu Tanggal 19 Juni Ya',
+    image: {
+      size: {
+        small: 'https://steffen-laurens.com/centennial/img/img-highlight-news-5.jpg',
+        medium: 'https://steffen-laurens.com/centennial/img/img-highlight-news-5.jpg'
+      },
+      caption: 'Oppo Find X'
+    },
+    writer: {
+      id: 'w5',
+      name: 'Vinny Agustin'
+    },
+    date: '3 Agustus 2018',
+    topics: ['Techno'],
+    url: '/techno/penasaran-oppo-find-x-tunggu-tanggal-19-juni-ya'
+  },
+  {
+    id: '6',
+    title: 'Jangan Ngaku Pecinta Serem Kalau Belum Liat Video Ini Di Youtube?',
+    image: {
+      size: {
+        small: 'https://steffen-laurens.com/centennial/img/img-highlight-news-6.jpg',
+        medium: 'https://steffen-laurens.com/centennial/img/img-highlight-news-6.jpg'
+      },
+      caption: 'The Ring"'
+    },
+    writer: {
+      id: 'w6',
+      name: 'Yusef Affendi'
+    },
+    date: '3 Agustus 2018',
+    topics: ['Pop Culture'],
+    url: '/pop-culture/jangan-ngaku-pecinta-serem-kalau-belum-liat-video-ini-di-youtube'
+  },
+  {
+    id: '7',
+    title: 'Film Believer Kalahkan Avengers Infinity War Dan Dead Pool Di Korea',
+    image: {
+      size: {
+        small: 'https://steffen-laurens.com/centennial/img/img-highlight-news-7.jpg',
+        medium: 'https://steffen-laurens.com/centennial/img/img-highlight-news-7.jpg'
+      },
+      caption: 'Film Korea Believer'
+    },
+    writer: {
+      id: 'w7',
+      name: 'Anzi Matta'
+    },
+    date: '2 Agustus 2018',
+    topics: ['Entertainment'],
+    url: '/entertainment/film-believer-kalahkan-avengers-infinity-war-dan-dead-pool-di-korea'
   }
 ];
 
--- src/home/Home.js
+++ src/home/Home.js
@@ -33,37 +33,43 @@ class ConnectedHome extends Component {
             {
               homeFetched &&
               users.map((user, index) => (
-                <div key={user.id} className={
-                  index === 0 ? 'col-50 main-col-featured-news' : 
-                  index === 1 || 2 ? 'col-50 secondary-col-featured-news' :
-                  'col-25 col-featured-news'
-                }>
-                  <div className="featured-news-box">
-                    <Link to={user.url} className="featured-news-image" title={user.title}>
-                      <img src={user.image.size.medium} alt={user.image.caption} />
-                    </Link>
-                    <div className={
-                      index === 0 ? 'main-summary featured-news-summary' :
-                      'featured-news-summary'
-                    }>
-                      <Link to={user.url} className="title-featured-news">{user.title}</Link>
-                      <div className="info-writer-box">
-                        <p className="writer">Ditulis oleh <Link to={user.url}>{user.writer.name}</Link></p>
-                        <p>{user.date}</p>
+                <React.Fragment>
+                  <div key={user.id} className={
+                    index === 0 ? 'col-50 main-col-featured-news' : 
+                    index === 1 || index === 2 ? 'col-50 secondary-col-featured-news' :
+                    index > 2 ? 'col-25 col-featured-news' : 'col-featured-news'
+                  }>
+                    <div className="featured-news-box">
+                      <Link to={user.url} className="featured-news-image" title={user.title}>
+                        <img src={user.image.size.medium} alt={user.image.caption} />
+                      </Link>
+                      <div className={
+                        index === 0 ? 'main-summary featured-news-summary' :
+                        index === 1 || index === 2 ? 'secondary-summary featured-news-summary' : 'featured-news-summary'
+                      }>
+                        <Link to={user.url} className="title-featured-news">{user.title}</Link>
+                        <div className="info-writer-box">
+                          <p className="writer">Ditulis oleh <Link to={user.url}>{user.writer.name}</Link></p>
+                          <p>{user.date}</p>
+                        </div>
+                        {
+                          user.topics.map((topic, index) => (
+                            <Link to={'/' + topic} key={index} className={
+                              topic === 'Pop Culture' ? 'pop-culture-topic topic' : 
+                              topic === 'Beauty & Fashion' ? 'beauty-topic topic' : 
+                              topic === 'Entertainment' ? 'entertainment topic' : 
+                              topic === 'Techno' ? 'techno topic' : 
+                              topic === 'Games' ? 'games topic' : 'topic'
+                            }>{topic}</Link>
+                          ))
+                        }
                       </div>
-                      {
-                        user.topics.map((topic, index) => (
-                          <Link to={'/' + topic} key={index} className={
-                            topic === 'Pop Culture' ? 'pop-culture-topic topic' : 
-                            topic === 'Beauty & Fashion' ? 'beauty-topic topic' : 
-                            topic === 'Entertainment' ? 'entertainment topic' :
-                            'topic'
-                          }>{topic}</Link>
-                        ))
-                      }
                     </div>
                   </div>
-                </div>
+                  {
+                    index === 2 || index === 6 ? <br /> : ''
+                  }
+                </React.Fragment>
               ))
             }
             {
