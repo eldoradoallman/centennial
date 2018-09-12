9f2474b15e9cc992aaeba44b9d4e9a0957c26078
Steffen Laurens <steffenlaurens@gmail.com>
2018-09-13 00:44:33 +0700

    change link on sidebarmenus

--- src/common/sidebarmenu/SidebarMenuComponent.js
+++ src/common/sidebarmenu/SidebarMenuComponent.js
@@ -8,19 +8,19 @@ const SidebarMenuComponent = ({ isSidebarOpen }) => (
         <NavLink to="/" exact activeClassName="selected">HOME</NavLink>
       </li>
       <li className="sidebar-menu">
-        <NavLink to="/about" exact activeClassName="selected">ENTERTAINMENT</NavLink>
+        <NavLink to="/category/entertainment" exact activeClassName="selected">ENTERTAINMENT</NavLink>
       </li>
       <li className="sidebar-menu">
-        <NavLink to="/topics" activeClassName="selected">LIVE STYLE</NavLink>
+        <NavLink to="/category/livestyle" activeClassName="selected">LIVE STYLE</NavLink>
       </li>
       <li className="sidebar-menu">
-        <NavLink to="/techno" exact activeClassName="selected">TECHNO</NavLink>
+        <NavLink to="/category/techno" exact activeClassName="selected">TECHNO</NavLink>
       </li>
       <li className="sidebar-menu">
-        <NavLink to="/about-you" exact activeClassName="selected">ABOUT YOU</NavLink>
+        <NavLink to="/category/about-you" exact activeClassName="selected">ABOUT YOU</NavLink>
       </li>
       <li className="sidebar-menu">
-        <NavLink to="/ideas" exact activeClassName="selected">IDEAS</NavLink>
+        <NavLink to="/category/ideas" exact activeClassName="selected">IDEAS</NavLink>
       </li>
     </ul>
   </div>
