85e42eb92ece5095f5eab9ed33877307bfa6ef24
Steffen Laurens <steffenlaurens@gmail.com>
2018-09-15 22:05:35 +0700

    move html elements from container into AuthorArticlesComponent

--- src/profile/authorarticles/AuthorArticles.js
+++ src/profile/authorarticles/AuthorArticles.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import axios from 'axios';
 import api from '../../api';
 
 import InfiniteScroll from 'react-infinite-scroll-component';
+import AuthorArticlesComponent from './AuthorArticlesComponent';
 
 import './AuthorArticles.css';
 
@@ -60,7 +60,6 @@ export default class AuthorArticles extends Component {
 
   render() {
     const { fetching, fetched, error, content, topic } = this.state;
-    console.log(this.state);
 
     return (
       <div className="history-content-wrapper">
@@ -81,32 +80,7 @@ export default class AuthorArticles extends Component {
             hasMore={true}
             loader={<p>Loading...</p>}
           >
-            {
-              content.map((article, index) => (
-                <div key={article.id} className="col-50 collection-articles">
-                  <div className="collection-articles-box">
-                    <div className="writer-info-top clear">
-                      <Link to={article.writer.url} className="writer-image">
-                        <img src={article.writer.avatar.small} alt={article.writer.name} />
-                      </Link>
-                      <div className="article-writer-info">
-                        <p className="writer">
-                          Ditulis oleh <Link to={article.writer.url}>{article.writer.name}</Link>
-                        </p>
-                        <p className="date">{article.date}</p>
-                      </div>
-                    </div>
-                    <Link to={article.url} className="article-image" title={article.title}>
-                      <img src={article.image.size.medium} alt={article.image.caption} />
-                    </Link>
-                    <div className="article-summary-wrapper">
-                      <Link to={article.url} className="title-article" title={article.title}>{article.title}</Link>
-                      <p className="article-summary-content">{article.summary.substring(0,150) + '...'}</p>
-                    </div>
-                  </div>
-                </div>
-              ))
-            }
+            <AuthorArticlesComponent content={content} />
           </InfiniteScroll>
         }
       </div>
---
+++ src/profile/authorarticles/AuthorArticlesComponent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const AuthorArticlesComponent = ({ content }) => (
+  content.map((article, index) => (
+    <div key={article.id} className="col-50 collection-articles">
+      <div className="collection-articles-box">
+        <div className="writer-info-top clear">
+          <Link to={article.writer.url} className="writer-image">
+            <img src={article.writer.avatar.small} alt={article.writer.name} />
+          </Link>
+          <div className="article-writer-info">
+            <p className="writer">
+              Ditulis oleh <Link to={article.writer.url}>{article.writer.name}</Link>
+            </p>
+            <p className="date">{article.date}</p>
+          </div>
+        </div>
+        <Link to={article.url} className="article-image" title={article.title}>
+          <img src={article.image.size.medium} alt={article.image.caption} />
+        </Link>
+        <div className="article-summary-wrapper">
+          <Link to={article.url} className="title-article" title={article.title}>{article.title}</Link>
+          <p className="article-summary-content">{article.summary.substring(0,150) + '...'}</p>
+        </div>
+      </div>
+    </div>
+  ))
+);
+
+export default AuthorArticlesComponent;
