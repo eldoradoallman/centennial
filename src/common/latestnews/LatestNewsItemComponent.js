dd608c4ace1be668d89fca136db0861830040a89
Steffen laurens <steffenlaurens@Steffens-iMac.local>
2018-11-11 16:40:14 +0700

    bug fix undefined state at home page

--- src/common/latestnews/LatestNewsItemComponent.js
+++ src/common/latestnews/LatestNewsItemComponent.js
@@ -9,31 +9,33 @@ const LatestNewsItemComponent = ({
     removeArticle,
     cancelToken
   }) => (
-  <div className="col-100 col-featured-news">
-    <div className="latest-news-box">
-      <Link to={article.url} className="latest-news-image" title={article.title}>
-        <img src={article.image.size.medium} alt={article.image.caption} />
-      </Link>
-      <div className="latest-news-summary">
-        <Link to={article.url} className="title-latest-news" title={article.title}>{article.title}</Link>
-        <p className="article-summary">{article.summary.substring(0,140) + '...'}</p>
-        <div className="latest-info-writer-box common">
-          <p className="writer">Ditulis oleh <Link to={article.writer.url}>{article.writer.name}</Link></p>
-          <p>{article.date}</p>
+  article.image ?
+    <div className="col-100 col-featured-news">
+      <div className="latest-news-box">
+        <Link to={article.url} className="latest-news-image" title={article.title}>
+          <img src={article.image.size.medium} alt={article.image.caption} />
+        </Link>
+        <div className="latest-news-summary">
+          <Link to={article.url} className="title-latest-news" title={article.title}>{article.title}</Link>
+          <p className="article-summary">{article.summary.substring(0,140) + '...'}</p>
+          <div className="latest-info-writer-box common">
+            <p className="writer">Ditulis oleh <Link to={article.writer.url}>{article.writer.name}</Link></p>
+            <p>{article.date}</p>
+          </div>
+          {
+            loggedIn &&
+            <ButtonBookmarks
+              user={user}
+              article={article}
+              action={article.isBookmarked ? removeArticle : addArticle}
+              cancelToken={cancelToken}
+              text={article.isBookmarked ? "Artikel Tersimpan" : "Simpan Artikel"}
+            />
+          }
         </div>
-        {
-          loggedIn &&
-          <ButtonBookmarks
-            user={user}
-            article={article}
-            action={article.isBookmarked ? removeArticle : addArticle}
-            cancelToken={cancelToken}
-            text={article.isBookmarked ? "Artikel Tersimpan" : "Simpan Artikel"}
-          />
-        }
       </div>
     </div>
-  </div>
+  : ''
 );
 
 const ButtonBookmarks = ({
